25c04ca6b87c466f0f4df9aa0aeb464c3a336d4d
boy-fc <954506111@qq.com>
2019-09-20 21:51:22 +0800

    黑马头条PC--接口访问-axios拦截器-统一处理异常数据

--- src/untils/axios-config.js
+++ src/untils/axios-config.js
@@ -1,5 +1,9 @@
 // 负责对axios进行处理
 import axios from 'axios'
+import { Message } from 'element-ui'
+
+import router from '../permisson'
+
 axios.defaults.baseURL = 'http://ttapi.research.itcast.cn/mp/v1_0' // 将地址的常态值设置给baseUrl
 // 请求拦截
 axios.interceptors.request.use(function (config) {
@@ -16,7 +20,32 @@ axios.interceptors.request.use(function (config) {
 axios.interceptors.response.use(function (response) {
   // 对响应数据进行处理，执行成功时进入
   return response.data ? response.data : {}
-}, function () {
-
+}, function (error) {
+  let status = error.response.status // 获取失败的状态码
+  let message = '未知错误'
+  switch (status) {
+    case 400:
+      message = '请求参数错误'
+      break
+    case 403:
+      message = '403 refresh_token未携带或已过期'
+      break
+    case 507:
+      message = '服务器数据库异常'
+      break
+    case 401:
+      message = 'token过期或未出'
+      window.localStorage.clear() // 清空缓存
+      router.push('/login') // this.$router.push()
+      break
+    case 404:
+      message = '手机号不正确'
+      break
+    default:
+      break
+  }
+  Message({ message })
+  //   希望 在异常处理函数中将所有的错误都处理完毕 不再进入catch  终止错误
+  return new Promise(function () {}) // 终止当前的错误
 })
 export default axios
