f948133e5dd873384b8103b24514b9466c9a4804
boy-fc <954506111@qq.com>
2019-09-21 09:56:23 +0800

    黑马头条PC-面包屑组件封装

---
+++ src/components/common/bread-crumb.vue
@@ -0,0 +1,20 @@
+<template>
+<!-- 分隔符 -->
+  <el-breadcrumb separator=">">
+  <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+  <el-breadcrumb-item>
+      <!-- 定义一个插槽 -->
+      <slot name="title"></slot>
+  </el-breadcrumb-item>
+</el-breadcrumb>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
