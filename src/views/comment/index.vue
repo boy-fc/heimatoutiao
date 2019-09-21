d95e9cfedaf845fd3a1f0d11385d23c0a1ba3b29
boy-fc <954506111@qq.com>
2019-09-21 19:47:48 +0800

    黑马头条PC-评论列表-页面结构-table的使用

--- src/views/comment/index.vue
+++ src/views/comment/index.vue
@@ -4,12 +4,25 @@
         <bread-crumb slot="header">
             <template slot="title">评论列表</template>
         </bread-crumb>
+        <!-- 表格列表 -->
+        <el-table :data="tableData">
+            <el-table-column label="标题" prop="title" width="600px"></el-table-column>
+            <el-table-column label="评论状态" prop="comment_status"></el-table-column>
+            <el-table-column label="总评论数" prop="total_comment_count"></el-table-column>
+            <el-table-column label="粉丝评论数" prop="fans_comment_count"></el-table-column>
+            <el-table-column label="操作"></el-table-column>
+        </el-table>
     </el-card>
+
 </template>
 
 <script>
 export default {
-
+  data () {
+    return {
+      tableData: []
+    }
+  }
 }
 </script>
 
