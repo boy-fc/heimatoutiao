7e9413781f8ede72225443828c98fcc8b399d4b9
boy-fc <954506111@qq.com>
2019-09-24 20:56:57 +0800

    黑马头条PC-发布文章-注册富文本编辑器并使用

--- src/components/index.js
+++ src/components/index.js
@@ -2,12 +2,19 @@
 import layoutAsile from './home/layout-asile'
 import layoutHeader from './home/layout-header'
 import breadCrumb from '../components/common/bread-crumb'
+// 富文本编辑
+import 'quill/dist/quill.core.css'
+import 'quill/dist/quill.snow.css'
+import 'quill/dist/quill.bubble.css'
+
+import { quillEditor } from 'vue-quill-editor'
 export default {
   install: function (Vue) {
     //    调用install方法 传入了Vue对象
     // 可不可以在实例化之前注册组件
     Vue.component('layout-asile', layoutAsile) // 全局注册左侧导航组件
     Vue.component('layout-header', layoutHeader) // 全局注册头部组件
-    Vue.component('bread-crumb', breadCrumb)
+    Vue.component('bread-crumb', breadCrumb)// 注册面包屑组件
+    Vue.component('quill-editor', quillEditor)// 注册富文本编辑器
   }
 }
--- src/views/publish/index.vue
+++ src/views/publish/index.vue
@@ -6,17 +6,17 @@
         </bread-crumb>
         <el-row class="titleCard">发表文章</el-row>
         <!-- 表单数据 -->
-        <el-form ref='publishForm' :model="formData" :rules="publishRules" style='margin-left:100px' label-width="100px">
+        <el-form ref='publishForm' :model="formData" :rules="publishRules" style='margin-left:50px' label-width="100px">
             <!-- 标题 -->
             <el-form-item prop='title' label="标题">
                 <el-input v-model="formData.title" style='width:400px'></el-input>
             </el-form-item>
             <!-- 内容 -->
             <el-form-item prop='content' label="内容">
-                <el-input  v-model="formData.content" type="textarea" :rows="4"  placeholder="请输入内容"></el-input>
+                <quill-editor  v-model="formData.content" style="height :300px"></quill-editor>
             </el-form-item>
             <!-- 封面 -->
-            <el-form-item label="封面" prop="cover">
+            <el-form-item label="封面" prop="cover" style="margin-top:120px">
                 <el-radio-group v-model="formData.cover.type">
                     <el-radio :label="1">单选</el-radio>
                     <el-radio :label="3">三图</el-radio>
