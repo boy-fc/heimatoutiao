5d0fcd9f5ec1966dd18b0496583af9f439696fab
boy-fc <954506111@qq.com>
2019-09-25 15:51:07 +0800

    黑马头条PC-账户信息-用户数据保存

--- src/views/account/index.vue
+++ src/views/account/index.vue
@@ -5,26 +5,26 @@
             <template slot='title'>账户信息</template>
         </bread-crumb>
          <!-- 主体 -->
-        <el-form style='margin-left:60px' label-width="100px">
-            <el-form-item label="用户名：">
+        <el-form style='margin-left:60px' label-width="100px" :model="formData" :rules="rules" ref="accountForm">
+            <el-form-item label="用户名：" prop='name'>
                 <el-input style="width:300px" v-model="formData.name"></el-input>
             </el-form-item>
             <el-form-item label="简介：">
                 <el-input style="width:300px" v-model="formData.intro"></el-input>
             </el-form-item>
-            <el-form-item label="邮箱：">
+            <el-form-item label="邮箱：" prop='email'>
                 <el-input style="width:300px" v-model="formData.email"></el-input>
             </el-form-item>
             <el-form-item label="手机号：">
                 <el-input style="width:300px" disabled v-model="formData.mobile"></el-input>
             </el-form-item>
             <el-form-item>
-                <el-button type="primary">保存信息</el-button>
+                <el-button type="primary" @click="saveUser">保存信息</el-button>
             </el-form-item>
             <!-- 头像 -->
             <div class="header">
-                 <img :src="formData.photo ? formData.photo : defaultImg" alt="" >
-                 <el-button type="text">修改头像</el-button>
+                <img :src="formData.photo ? formData.photo : defaultImg" alt="" >
+                <el-button type="text">修改头像</el-button>
             </div>
 
         </el-form>
@@ -36,7 +36,18 @@ export default {
   data () {
     return {
       formData: {},
-      defaultImg: require('../../assets/img/fc.jpg')
+      defaultImg: require('../../assets/img/fc.jpg'),
+      //   表单校验
+      rules: {
+        name: [
+          { required: true, message: '请输入用户名', trigger: 'blur' },
+          { min: 1, max: 7, message: '用户名在在 1 到 7 个字符', trigger: 'blur' }
+        ],
+        email: [
+          { required: true, message: '邮箱不能为空' },
+          { pattern: /^\w+((.\w+)|(-\w+))@[A-Za-z0-9]+((.|-)[A-Za-z0-9]+).[A-Za-z0-9]+$/, message: '邮箱格式不正确' }
+        ]
+      }
     }
   },
   methods: {
@@ -46,6 +57,22 @@ export default {
         url: '/user/profile'
       }).then(result => {
         this.formData = result.data
+      })
+    },
+    // 保存信息
+    saveUser () {
+      // 校验整个表单
+      this.$refs.accountForm.validate((isOk) => {
+        this.$axios({
+          url: '/user/profile',
+          method: 'patch',
+          data: this.formData
+        }).then(() => {
+          this.$message({
+            message: '保存成功',
+            type: 'success'
+          })
+        })
       })
     }
   },
